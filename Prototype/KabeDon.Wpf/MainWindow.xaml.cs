b06ac19765a7745929434eeb0c6716ce1c39e830
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-02-07 17:45:24 +0900

    帽子と禁止領域くらいは領域追加

--- Prototype/KabeDon.Wpf/MainWindow.xaml.cs
+++ Prototype/KabeDon.Wpf/MainWindow.xaml.cs
@@ -11,15 +11,25 @@ namespace KabeDon.Wpf
             CloudiaImage.MouseDown += CloudiaImage_MouseDown;
         }
 
+        private Rect Hat = new Rect(200, 146, 176, 94);
         private Rect Face = new Rect(203, 233, 153, 137);
+        private Rect Forbidden = new Rect(142, 413, 182, 547);
 
         private void CloudiaImage_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             var position = e.MouseDevice.GetPosition(CloudiaImage);
 
-            if (Face.Contains(position))
+            if (Forbidden.Contains(position))
             {
-                MessageBox.Show($"顔");
+                System.Diagnostics.Debug.WriteLine("禁止領域");
+            }
+            else if (Face.Contains(position))
+            {
+                MessageBox.Show("顔");
+            }
+            else if (Hat.Contains(position))
+            {
+                MessageBox.Show("帽子");
             }
             else
             {
