d5ff0dbbf46b6fbd88a4a5bfd2f761e005c13962
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-02-07 20:30:09 +0900

    それっぽく音を呼び分け

---
+++ Prototype/KabeDon.Wpf/AreaInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace KabeDon.Wpf
+{
+    internal class AreaInfo
+    {
+        public MediaElement Sound { get; }
+        public Rect Rect { get; }
+
+        public AreaInfo(Rect rect, MediaElement sound)
+        {
+            this.Rect = rect;
+            this.Sound = sound;
+        }
+    }
+}
--- Prototype/KabeDon.Wpf/MainWindow.xaml.cs
+++ Prototype/KabeDon.Wpf/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
 
 namespace KabeDon.Wpf
 {
@@ -8,33 +10,54 @@ namespace KabeDon.Wpf
         {
             InitializeComponent();
 
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            _areaSettings = new[]
+            {
+                new AreaInfo(new Rect(379, 301, 58, 66), ExcellentSound),
+                new AreaInfo(new Rect(393, 210, 128, 181), GoodSound),
+                new AreaInfo(new Rect(468, 405, 41, 53), EasterEggSound),
+                new AreaInfo(new Rect(200, 146, 176, 94), HatSound),
+                new AreaInfo(new Rect(203, 233, 153, 137), FaceSound),
+            };
+
             CloudiaImage.MouseDown += CloudiaImage_MouseDown;
         }
 
+        private AreaInfo[] _areaSettings;
+
+        private Rect ExcellentKabeDon = new Rect(379, 301, 58, 66);
+        private Rect GoodKabeDon = new Rect(393, 210, 128, 181);
+        private Rect EasterEgg = new Rect(468, 405, 41, 53);
         private Rect Hat = new Rect(200, 146, 176, 94);
         private Rect Face = new Rect(203, 233, 153, 137);
-        private Rect Forbidden = new Rect(142, 413, 182, 547);
+        private Rect ForbiddenArea = new Rect(142, 413, 182, 547);
 
         private void CloudiaImage_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             var position = e.MouseDevice.GetPosition(CloudiaImage);
 
-            if (Forbidden.Contains(position))
+            if (ForbiddenArea.Contains(position))
             {
                 System.Diagnostics.Debug.WriteLine("禁止領域");
             }
-            else if (Face.Contains(position))
+
+            foreach (var area in _areaSettings)
             {
-                MessageBox.Show("顔");
+                if (area.Rect.Contains(position))
+                {
+                    System.Diagnostics.Debug.WriteLine($"プレイ { position.X}, { position.Y}");
+                    area.Sound.Position = TimeSpan.Zero;
+                    area.Sound.Play();
+                    return;
+                }
             }
-            else if (Hat.Contains(position))
-            {
-                MessageBox.Show("帽子");
-            }
-            else
-            {
-                MessageBox.Show($"他の場所クリック {position.X}, {position.Y}");
-            }
+
+            System.Diagnostics.Debug.WriteLine($"他の場所クリック { position.X}, { position.Y}");
         }
     }
 }
