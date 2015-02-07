c7723f09d2298fa648a68661bbb95b625591f7c9
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-02-07 17:24:02 +0900

    クリック地点を拾うところまで

--- Prototype/KabeDon.Wpf/MainWindow.xaml.cs
+++ Prototype/KabeDon.Wpf/MainWindow.xaml.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace KabeDon.Wpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            CloudiaImage.MouseDown += CloudiaImage_MouseDown;
+        }
+
+        private void CloudiaImage_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var position = e.MouseDevice.GetPosition(CloudiaImage);
+            MessageBox.Show($"クリック {position.X}, {position.Y}");
         }
     }
 }
