041664a2b848ae86659d132178ccce47fa5d05e6
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-03-09 23:52:45 +0900

    仮データはJPG/MP3に

--- Ideal/KabeDon/KabeDon.XamarinForms/KabeDon.XamarinForms.Droid/MainActivity.cs
+++ Ideal/KabeDon/KabeDon.XamarinForms/KabeDon.XamarinForms.Droid/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.IO;
 
 namespace KabeDon.XamarinForms.Droid
 {
@@ -14,6 +15,9 @@ namespace KabeDon.XamarinForms.Droid
     {
         protected override void OnCreate(Bundle bundle)
         {
+            //Android.OS.Environment.DataDirectory.
+            //FileSystemInfo
+            //System.Environment
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
--- Ideal/KabeDon/KabeDon.XamarinForms/KabeDon.XamarinForms.iOS/Main.cs
+++ Ideal/KabeDon/KabeDon.XamarinForms/KabeDon.XamarinForms.iOS/Main.cs
@@ -12,6 +12,9 @@ namespace KabeDon.XamarinForms.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            //var s = System.IO.File.OpenRead("./"); // これで、アプリのコンテンツ
+            //var s = System.IO.File.OpenRead(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)); // これでアプリのローカル ストレージ
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
