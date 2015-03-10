29a05ca64fa0bb618097f4a7e9b66c65f31b7b7b
Nobuyuki Iwanaga <ufcpp@live.jp>
2015-03-11 01:29:55 +0900

    サーバー(仮)
    認証もくそもなく、App_Data に入ってる ZIP をただ送り返すだけのサーバー。
    今、App_Data には SampleCloudia.zip だけ入ってる。

---
+++ Ideal/KabeDon/KabeDon.Server/Controllers/LevelController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace KabeDon.Server.Controllers
+{
+    public class LevelController : Controller
+    {
+        //todo: 認証付ける
+
+        // GET: Level
+        public ActionResult Index(string name)
+        {
+            // ファイル読み込み非同期にした方がいいかも。
+            var path = Server.MapPath("~/App_Data/" + name + ".zip");
+            return File(path, "application/zip");
+        }
+    }
+}
---
+++ Ideal/KabeDon/KabeDon.Server/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KabeDon.Server.MvcApplication" Language="C#" %>
