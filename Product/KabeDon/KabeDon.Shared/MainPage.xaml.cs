3ac8b41383c9f811d86cb3f7b62dad611a0cba08
niwanaga <iwanaga@orange-cube.jp>
2015-04-15 10:49:39 +0900

    【本誌コード】 メソッド抽出

--- Product/KabeDon/KabeDon.Shared/MainPage.xaml.cs
+++ Product/KabeDon/KabeDon.Shared/MainPage.xaml.cs
@@ -30,39 +30,13 @@ namespace KabeDon
         {
             this.InitializeComponent();
 
-            var areas = new[]
-            {
-                new Area { X = 396, Y = 238, Width = 320, Height =  108, Image = "Cloudia4", Sound = "chui" },
-                new Area { X = 365, Y = 215, Width = 380, Height =  529, Image = "Cloudia2", Sound = "ah" },
-                new Area { X = 276, Y = 340, Width =  65, Height =  415, Image = "Cloudia3", Sound = "hai" },
-                new Area { X = 739, Y = 340, Width =  65, Height =  415, Image = "Cloudia3", Sound = "hai" },
-                new Area { X = 271, Y = 772, Width = 590, Height = 1148, Image = "Cloudia4", Sound = "chui" },
-            };
-
             image.Tapped += async (sender, arg) =>
             {
                 image.IsTapEnabled = false;
 
                 var pos = arg.GetPosition(image);
 
-                // 今の座標 ÷ 表示されている画像サイズ × 元の画像サイズ
-                var x = pos.X / image.ActualWidth * 1080;
-                var y = pos.Y / image.ActualHeight * 1920;
-
-                foreach (var area in areas)
-                {
-                    if (area.X < x && x < area.X + area.Width
-                        && area.Y < y && y < area.Y + area.Height)
-                    {
-                        var mediaUri = new Uri($"ms-appx:///Assets/Sound/{area.Sound}.mp3");
-                        await PlaySound(mediaUri);
-
-                        var imageUri = new Uri($"ms-appx:///Assets/Image/{area.Image}.png");
-                        ShowImage(imageUri);
-
-                        break;
-                    }
-                }
+                HitTest(pos);
 
                 await Task.Delay(1000);
                 image.IsTapEnabled = true;
@@ -70,6 +44,41 @@ namespace KabeDon
                 ShowImage(new Uri("ms-appx:///Assets/Image/Cloudia1.png"));
             };
         }
+
+        private void HitTest(Point pos)
+        {
+            // 今の座標 ÷ 表示されている画像サイズ × 元の画像サイズ
+            var x = pos.X / image.ActualWidth * 1080;
+            var y = pos.Y / image.ActualHeight * 1920;
+
+            foreach (var area in areas)
+            {
+                if (area.X < x && x < area.X + area.Width
+                    && area.Y < y && y < area.Y + area.Height)
+                {
+                    OnHit(area);
+                    break;
+                }
+            }
+        }
+
+        private void OnHit(Area area)
+        {
+            var mediaUri = new Uri($"ms-appx:///Assets/Sound/{area.Sound}.mp3");
+            PlaySound(mediaUri);
+
+            var imageUri = new Uri($"ms-appx:///Assets/Image/{area.Image}.png");
+            ShowImage(imageUri);
+        }
+
+        static readonly Area[] areas = new[]
+        {
+            new Area { X = 396, Y = 238, Width = 320, Height =  108, Image = "Cloudia4", Sound = "chui" },
+            new Area { X = 365, Y = 215, Width = 380, Height =  529, Image = "Cloudia2", Sound = "ah" },
+            new Area { X = 276, Y = 340, Width =  65, Height =  415, Image = "Cloudia3", Sound = "hai" },
+            new Area { X = 739, Y = 340, Width =  65, Height =  415, Image = "Cloudia3", Sound = "hai" },
+            new Area { X = 271, Y = 772, Width = 590, Height = 1148, Image = "Cloudia4", Sound = "chui" },
+        };
 
         private async void ShowImage(Uri imageUri)
         {
@@ -80,7 +89,7 @@ namespace KabeDon
             image.Source = bitmap;
         }
 
-        private static async Task PlaySound(Uri mediaUri)
+        private static async void PlaySound(Uri mediaUri)
         {
             var file = await StorageFile.GetFileFromApplicationUriAsync(mediaUri);
             var stream = await file.OpenReadAsync();
